dc94abf97d68134edebc0543dc5461f6e134f70e
Samy Belaloui-Bertot <samy.belaloui@bellman.immo>
2022-07-12 18:21:11 +0200

    feat: round generator, day information + add crons

--- src/bin/crons.ts
+++ src/bin/crons.ts
@@ -7,13 +7,13 @@ import { main as roundGeneratorMain } from './round-generator/roundGenerator'
 export const crons = () => {
   // Round generation
   // Every day at 5:55AM
-  cron.schedule('55 5 * * *', function () {
+  cron.schedule('55 5 * * *', () => {
     prismaInjector(roundGeneratorMain)
   })
 
   // Day information
-  // Eveery day at 6AM
-  cron.schedule('0 6 * * *', function () {
+  // Every day at 6AM
+  cron.schedule('0 6 * * *', () => {
     prismaInjector(dayInformationMain)
   })
 }
